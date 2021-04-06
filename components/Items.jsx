2749f09453b6e24716ed3dbb5d3391396183f899
kiyo <kiyonari6318@yahoo.co.jp>
2021-04-06 23:31:14 +0900

    Fix: トップページプロジェクトと詳細紐付け

--- components/Items.jsx
+++ components/Items.jsx
@@ -8,13 +8,25 @@ export default function Items() {
       <MediaQuery query="(min-width: 767px)">
         <div className={styles.pc_container}>
           <div className={styles.div1}>
-            <img src="/images/001.png" />
+            <Link href="/projects/freestarsinterior">
+              <a>
+                <img src="/images/001.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div2}>
-            <img src="/images/002.png" />
+            <Link href="/projects/kadomatsutrees">
+              <a>
+                <img src="/images/002.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div3}>
-            <img src="/images/003.png" />
+            <Link href="/projects/forplusgroup">
+              <a>
+                <img src="/images/003.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div4}>
             <Link href="/projects">
@@ -24,10 +36,18 @@ export default function Items() {
             </Link>
           </div>
           <div className={styles.div5}>
-            <img src="/images/006.png" />
+            <Link href="/projects/arch">
+              <a>
+                <img src="/images/006.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div6}>
-            <img src="/images/007.png" />
+            <Link href="/projects/freestarsbrand">
+              <a>
+                <img src="/images/007.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div7}>
             <Link href="/contact">
@@ -37,10 +57,18 @@ export default function Items() {
             </Link>
           </div>
           <div className={styles.div8}>
-            <img src="/images/005.png" />
+            <Link href="/projects/brilliantbiceps">
+              <a>
+                <img src="/images/005.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div9}>
-            <img src="/images/008.png" />
+            <Link href="/projects/noginoai">
+              <a>
+                <img src="/images/008.png" />
+              </a>
+            </Link>
           </div>
           <div className={styles.div10 + ' ' + styles.project_list}>
             <Link href="/projects">
